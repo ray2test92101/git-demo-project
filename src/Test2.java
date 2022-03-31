1b89316a194e5aa9ac71d854649b9dc6cb65b0e9
ray2test92101 <ray2test92101@gmail.com>
2022-03-30 21:05:42 -0700

    Updated Test 2. Copy the main method in Test 1. Add a condition for if else "no" Add a condition for else.

--- src/Test2.java
+++ src/Test2.java
@@ -4,5 +4,14 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello ");
+		
+String hungry = "yes";
+		
+		if(hungry == "yes")
+			System.out.println("eat something.....");
+		else if(hungry == "no")
+			System.out.println("Do work ...........");
+		else
+			System.out.println("You must be from another planet!");
 	}
 }
